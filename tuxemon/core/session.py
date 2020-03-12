6aaa6cd6b46d34ca3471743a6a5684314f1efe48
Leif Theden <leif.theden@gmail.com>
2020-03-12 18:55:36 -0400

    session docstrings

--- tuxemon/core/session.py
+++ tuxemon/core/session.py
@@ -3,12 +3,19 @@ class Session(object):
 
     Contains Control, World, and Player
 
+    Eventually this will be extended to support network sessions
+
     """
     def __init__(self, control, world, player):
+        """
+        :param tuxemon.core.control.Control: Game client
+        :param tuxemon.core.world.World: Game world
+        :param tuxemon.core.player.Player: Player object
+        """
         self.control = control
         self.world = world
         self.player = player
 
 
-# will be filled in later when game starts
+# WIP will be filled in later when game starts
 local_session = Session(None, None, None)
